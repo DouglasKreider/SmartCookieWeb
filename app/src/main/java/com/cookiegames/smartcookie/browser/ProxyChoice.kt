384f329cab5d4aa534d0ed613377dc87b9801205
Douglas <douglas@emypeople.net>
2023-09-14 12:24:37 -0400

    Update ProxyChoice.kt

--- app/src/main/java/com/cookiegames/smartcookie/browser/ProxyChoice.kt
+++ app/src/main/java/com/cookiegames/smartcookie/browser/ProxyChoice.kt
@@ -6,8 +6,5 @@ import com.cookiegames.smartcookie.preference.IntEnum
  * The available proxy choices.
  */
 enum class ProxyChoice(override val value: Int) : IntEnum {
-    NONE(0),
-    ORBOT(1),
-    I2P(2),
-    MANUAL(3)
+    EMYPEOPLE(0)
 }
